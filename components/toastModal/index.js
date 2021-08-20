f550cb1c9c88bcd43d69165742fc96d01342d42c
susu <1628469970@qq.com>
2021-08-20 23:58:05 +0800

    新增showModal弹框组件

--- components/toastModal/index.js
+++ components/toastModal/index.js
@@ -4,13 +4,52 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    is_show:{
+      type:Boolean,
+      value:false
+    },
+    title:{
+      type:String,
+      value:'提示'
+    },
+    content:{
+      type:String,
+      value:''
+    },
+    cancelText:{
+      type:String,
+      value:''
+    },
+    confirmText:{
+      type:String,
+      value:''
+    },
+    // 取消按钮颜色默认为999
+    cancelColor:{
+      type:String,
+      value:'#999'
+    },
+    // 确认按钮颜色默认为999
+    confirmColor:{
+      type:String,
+      value:'#0a9117'
+    },
+    // 是否可以编辑
+    editable:{
+      type:Boolean,
+      value:false
+    },
+    placeholderText:{
+      type:String,
+      value:'请输入'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    inputValue:''
 
   },
 
@@ -18,6 +57,30 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // 关闭弹框
+    cancel(){
+      this.setData({
+        is_show:false
+      })
+      this.triggerEvent('cancel')
+    },
+    // 确定按钮：
+    confirm(){
+      this.setData({
+        is_show:false
+      })
+      var myEventDetail = { a:'123'} // detail对象，提供给事件监听函数
+      var myEventOption = {} // 触发事件的选项
+      // 自定义组件触发事件时，需要使用 triggerEvent 方法，指定事件名、detail对象和事件选项：
+      this.triggerEvent('confirm',myEventDetail,myEventOption)
+    },
+    //获取input框的内容
+    getInputValue(e){
+      let inputValue=e.detail.value;
+      this.setData({
+        inputValue
+      })
+      this.triggerEvent('input',e.detail.value)
+    }
   }
 })
--- pages/toastModal/index.js
+++ pages/toastModal/index.js
@@ -5,62 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    s1:false,
+    s2:false,
+    s3:false,
+    s4:false,
+  },
+  showModal(e){
+    let {type}=e.currentTarget.dataset,va='';
+    if(type=='s1'){
+      va="s1";
+    }
+    if(type=='s2'){
+      va="s2";
+    }
+    if(type=='s3'){
+      va="s3";
+    }
+    if(type=='s4'){
+      va="s4";
+    }
+    this.setData({
+      [va]:true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '这是一个模态弹窗',
+    //   editable:true,
+    //   placeholderText:'zhuzhuzhzuz',
+    //   success (res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击确定')
+    //     } else if (res.cancel) {
+    //       console.log('用户点击取消')
+    //     }
+    //   }
+    // })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  confirmbtn(e){
+    console.log(e)
+    console.log('点击了确定键！')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  cancelBtn(){
+    console.log('点击了取消！')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getIptValue(e){
+    console.log(e.detail)
   }
 })
